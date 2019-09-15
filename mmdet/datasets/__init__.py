a7fec24f53fcd99e0baff0f150141dff0ec479e7
Oskar Holmberg <oskarholmberg94@gmail.com>
2019-09-15 16:48:40 +0200

    Added ConsiditionDataset to available datasets

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -2,6 +2,7 @@ from .builder import build_dataset
 from .cityscapes import CityscapesDataset
 from .coco import CocoDataset
 from .custom import CustomDataset
+from .considition import ConsiditionDataset
 from .dataset_wrappers import ConcatDataset, RepeatDataset
 from .extra_aug import ExtraAugmentation
 from .loader import DistributedGroupSampler, GroupSampler, build_dataloader
@@ -14,5 +15,5 @@ __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
     'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'build_dataloader', 'ConcatDataset', 'RepeatDataset', 'ExtraAugmentation',
-    'WIDERFaceDataset', 'DATASETS', 'build_dataset'
+    'WIDERFaceDataset', 'DATASETS', 'build_dataset', 'ConsiditionDataset'
 ]
